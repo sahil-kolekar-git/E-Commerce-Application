572e38c62cad43a60007258ee1dba93acff0e7cd
sahil-kolekar-git <sahilkolekar.02@gmail.com>
2025-06-09 12:45:22 +0530

    creating navbar

---
+++ e-commerce-app-frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar it is</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ e-commerce-app-frontend/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home.jsx";
+
+let routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={routes}>
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
